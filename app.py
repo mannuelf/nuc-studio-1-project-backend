e69ad1c5ffe685f3dced6db9c7bf70c445b7e76e
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-02-21 20:45:18 +0100

    wip: got the db creation function ready, some bug reading the .xsl file

--- app.py
+++ app.py
@@ -1,29 +1,49 @@
 import os
+
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+
 import pandas as pd
 import sqlite3
 import xlrd
 
+
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
-@app.route('/', methods=['GET'])
-def get():
-    return jsonify({'message': 'Hello world'})
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    os.path.join(basedir, 'db.sqlite')
+  os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)  # Init DB
 ma = Marshmallow(app)  # Init Marshmallow
 
+def db_create():
+    db_connect = sqlite3.connect('./db.sqlite')
+    connect = db_connect.cursor()
 
-# Hello World Class/Model
+    # Population Levels
+    pl_data = pd.read_excel('data/factbook-2015-table1-en.xls', sheet_name='population_levels', header=0, engine='openpyxl')
+    pl_data.to_sql('population_levels', db_connect, if_exists='append', index=False)
+
+    SQL = '''CREATE TABLE population_levels (CountryID INTEGER,
+                Country TEXT NOT NULL,
+                Year TEXT NOT NULL,
+                PRIMARY KEY (CountryID))'''
+
+    connect.execute(SQL)
+    pl_data.to_sql('population_levels', db_connect, if_exists='append', index=False)
+    connect.close()
+
+
+@app.route('/', methods=['GET'])
+
+def get():
+    db_create()
+    return jsonify({'message': 'Hello world'})
+
+
 class HelloWorld(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(100), unique=True)
@@ -34,7 +54,6 @@ class HelloWorld(db.Model):
         self.description = description
 
 
-# Hello World Schema
 class HelloWorldSchema(ma.SQLAlchemySchema):
     class Meta:
         fields = ('id', 'message', 'description')
@@ -44,8 +63,6 @@ class HelloWorldSchema(ma.SQLAlchemySchema):
 hello_world_schema = HelloWorldSchema()
 hello_worlds_schema = HelloWorldSchema(many=True)
 
-
-# Create message
 @app.route('/hello-world', methods=['POST'])
 def add_message():
     message = request.json['message']
@@ -59,7 +76,6 @@ def add_message():
     return hello_world_schema.jsonify(new_message)
 
 
-# Get all messages
 @app.route('/hello-world', methods=['GET'])
 def get_messages():
     all_messages = HelloWorld.query.all()
@@ -67,12 +83,34 @@ def get_messages():
     return jsonify(result)
 
 
-# Get one message
 @app.route('/hello-world/<id>', methods=['GET'])
 def get_message(id):
     message = HelloWorld.query.get(id)
     result = hello_world_schema.dump(message)
     return jsonify(result)
+
+'''
+class PopulationLevels():
+  __tablename__ = "population_levels"
+  CountryId = Column(Integer, primary_key=True)
+  Country = Column(String)
+  Year = Column(String)
+
+class PopulationLevelsSchema(ma.Schema):
+    class Meta:
+        fields = ('CountryId', 'Country', 'Year')
+
+@app.route('/population-levels', method=['GET'])
+def get_population_levels():
+  print("getting population levels")
+  #data = PopulationLevels.query.get()
+  #result = population_levels_schema.dump(data)
+  #return jsonify(result)
+'''
+
+# Init the schema
+hello_world_schema = HelloWorldSchema()
+hello_worlds_schema = HelloWorldSchema(many=True)
 
 
 if __name__ == '__main__':
