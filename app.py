2e9a6e8f312e2da1136e338b1ec7fc960ba4b30b
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-03-13 08:49:30 +0100

    wip: database table import script working

--- app.py
+++ app.py
@@ -22,12 +22,22 @@ def db_create_population_levels():
         db_connect = sqlite3.connect('data/db.sqlite')
         cur = db_connect.cursor()
 
-        SQL = '''CREATE TABLE population_levels (CountryID INTEGER,
+        SQL = '''CREATE TABLE IF NOT EXISTS population_levels (CountryID INTEGER,
                 Country TEXT NOT NULL,
-                Year TEXT NOT NULL,
+                [2002] TEXT NOT NULL,
+                [2003] TEXT NOT NULL,
+                [2004] TEXT NOT NULL,
+                [2005] TEXT NOT NULL,
+                [2006] TEXT NOT NULL,
+                [2007] TEXT NOT NULL,
+                [2008] TEXT NOT NULL,
+                [2009] TEXT NOT NULL,
+                [2010] TEXT NOT NULL,
+                [2011] TEXT NOT NULL,
+                [2012] TEXT NOT NULL,
+                [2013] TEXT NOT NULL,
+                [2014] TEXT NOT NULL,
                 PRIMARY KEY (CountryID))'''
-
-        INSERTSQL = '''INSTER INTO population_levels (CountryID, Country, Year) VALUES (?,?,?)'''
         cur.execute(SQL)
         db_connect.close()
     except sqlite3.Error as error:
@@ -41,15 +51,17 @@ def db_insert_population_levels():
         db_connect = sqlite3.connect('data/db.sqlite')
         cur = db_connect.cursor()
 
-        print("connected to db")
+        print("🚀 connected to db")
 
-        SQL = '''INSERT INTO population_levels (CountryID, Country, Year) VALUES (?,?,?)'''
+        SQL = '''INSERT INTO population_levels (Country, Year) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''
 
-        population_levels_csv = open('data/factbook-2015-table1-en.csv')
-        rows = csv.reader(population_levels_csv)
-
-        cur.executemany(SQL, rows)
-        print(cur.fetchall())
+        population_levels_csv = pd.read_csv('data/factbook-2015-table1-en.csv', engine='python', encoding="UTF-8", header=0, delimiter=";", skiprows=3, skipfooter=1, index_col=0)
+        df_drop_last_2_rows = population_levels_csv.iloc[:-1]
+        df_drop_last_2_rows.columns.values[0] = "Country"
+        print(df_drop_last_2_rows)
+        df_drop_last_2_rows.to_sql('population_levels', db_connect, if_exists='append', index=False)
+        cur.execute("SELECT * FROM population_levels")
+        print("💿", cur.fetchall())
         db_connect.commit()
         db_connect.close()
     except sqlite3.Error as error:
@@ -61,8 +73,8 @@ def db_insert_population_levels():
 
 @app.route('/', methods=['GET'])
 def get():
+    db_create_population_levels()
     db_insert_population_levels()
-    # db_create() # TODO for testing create the DB, make this better.
     return jsonify({'message': 'Hello world'})
 
 
@@ -109,7 +121,6 @@ def get_message(id):
 
 hello_world_schema = HelloWorldSchema()
 hello_worlds_schema = HelloWorldSchema(many=True)
-
 
 
 if __name__ == '__main__':
